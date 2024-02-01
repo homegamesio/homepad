18d60c8e17afd1e5ea114c5b4e4f57f2b0395ca9
Joseph Garcia <prosif@gmail.com>
2024-01-31 20:56:38 -0700

    add stadia

--- mappings.js
+++ mappings.js
@@ -1,5 +1,7 @@
 const dualshock4 = require('./gamepad-mappings/ds4');
+const stadia = require('./gamepad-mappings/stadia');
 
 module.exports = {
-    'DUALSHOCK_4': dualshock4
+    'DUALSHOCK_4': dualshock4,
+    'STADIA': stadia
 };
