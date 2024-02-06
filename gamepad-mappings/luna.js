5aedc973b6267001d8b1e00c42dd2bd0651d5d2e
Joseph Garcia <prosif@gmail.com>
2024-02-05 19:35:22 -0700

    more

---
+++ gamepad-mappings/luna.js
@@ -0,0 +1,34 @@
+module.exports = {
+    name: 'Luna Gamepad Extended Gamepad',
+    buttons: {
+        'FACE_1': 0,
+        'FACE_2': 1,
+        'FACE_3': 2,
+        'FACE_4': 3,
+        'LEFT_SHOULDER': 4,
+        'RIGHT_SHOULDER': 5,
+        'LEFT_TRIGGER': 6,
+        'RIGHT_TRIGGER': 7,
+        'SELECT': 8,
+        'START': 9,
+        'STICK_CLICK_1': 10,
+        'STICK_CLICK_2': 11,
+        'DIRECTION_UP': 12,
+        'DIRECTION_DOWN': 13,
+        'DIRECTION_LEFT': 14,
+        'DIRECTION_RIGHT': 15,
+        'HOME': 16,
+    },
+    sticks: {
+        'STICK_1_X': 0,
+        'STICK_1_Y': 1,
+        'STICK_2_X': 2,
+        'STICK_2_Y': 3,
+    },
+    deadzones: {
+        'STICK_1_X': 0.08,
+        'STICK_1_Y': 0.08,
+        'STICK_2_X': 0.08,
+        'STICK_2_Y': 0.08
+    }
+}
--- mappings.js
+++ mappings.js
@@ -1,7 +1,17 @@
 const dualshock4 = require('./gamepad-mappings/ds4');
 const stadia = require('./gamepad-mappings/stadia');
+const xboxOne = require('./gamepad-mappings/xbox-one');
+const luna = require('./gamepad-mappings/luna');
+const dualshock5 = require('./gamepad-mappings/ds5');
+const switchPro = require('./gamepad-mappings/switch-pro');
+const nes30 = require('./gamepad-mappings/nes30');
 
 module.exports = {
     'DUALSHOCK_4': dualshock4,
-    'STADIA': stadia
+    'STADIA': stadia,
+    'XBOX_ONE': xboxOne,
+    'LUNA': luna,
+    'DUALSHOCK_5': dualshock5,
+    'SWITCH_PRO': switchPro,
+    'NES_30': nes30
 };
